a6d13bcb761e786134dfbfa1c4897c92eeac746a
rhiodam <rhioda.m@gmail.com>
2019-03-23 18:32:34 +0700

    Add bookstore ui

---
+++ bookstore-ui/src/main/java/com/rhiodamuthie/bookstoreui/BookstoreUiApplication.java
@@ -0,0 +1,13 @@
+package com.rhiodamuthie.bookstoreui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreUiApplication.class, args);
+    }
+
+}
